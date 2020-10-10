ea5112ba1c61d3c12e9697a6ff8f979a5a6cc857
mikeda <baguzy@gmail.com>
2020-10-11 08:55:41 +0900

    Previousボタン

--- src/App.js
+++ src/App.js
@@ -38,6 +38,15 @@ class App extends Component {
     })
   }
 
+  goPrevious(search) {
+    this.setState({
+      first: null,
+      after: null,
+      last: PER_PAGE,
+      before: search.pageInfo.startCursor
+    })
+  }
+
   render() {
     const { query, first, last, before, after } = this.state;
 
@@ -80,6 +89,16 @@ class App extends Component {
                       })
                     }
                   </ul>
+                  {
+                    search.pageInfo.hasPreviousPage === true ?
+                    <button
+                      onClick={this.goPrevious.bind(this, search)}
+                    >
+                      Previous
+                    </button>
+                    : null
+                  }
+
                   {
                     search.pageInfo.hasNextPage === true ?
                     <button
